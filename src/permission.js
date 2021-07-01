042c481ece7501e3a2f032e1ec78ff15ee49e578
yangzheli <1936558266@qq.com>
2021-07-01 19:10:43 +0800

    :sparkles: permission.js

--- src/permission.js
+++ src/permission.js
@@ -3,9 +3,28 @@ import router from './router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
+import store from '@/store/index.js'
+
+const whiteList = ['/login'] // no redirect whiteList
+
 router.beforeEach((to, from, next) => {
     NProgress.start()
-    next()
+
+    const token = store.state.user.token
+    console.log(token)
+    if (token) {
+        if (to.path === '/login') {
+            next({ path: '/' })
+            NProgress.done()
+        } else {
+            next()
+            NProgress.done()
+        }
+    } else {
+        if (whiteList.indexOf(to.path) !== -1) next()
+        else next(`/login?redirect=${to.path}`)
+        NProgress.done()
+    }
 })
 
 router.afterEach(() => {
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,5 @@
 const getters = {
+    token: state => state.user.token,
     sidebar: state => state.app.sidebar,
     device: state => state.app.device
 }
